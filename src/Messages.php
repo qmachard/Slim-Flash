de55bf813124f255a64fd348ce101290b063931c
Quentin MACHARD <quentin.machard@palpix.com>
2016-07-08 10:50:00 +0200

    Feat. Add message for current request

--- src/Messages.php
+++ src/Messages.php
@@ -1,10 +1,8 @@
 <?php
 namespace Slim\Flash;
-
 use ArrayAccess;
 use RuntimeException;
 use InvalidArgumentException;
-
 /**
  * Flash messages
  */
@@ -16,28 +14,30 @@ class Messages
      * @var string[]
      */
     protected $fromPrevious = [];
-
+    /**
+     * Messages for current request
+     *
+     * @var string[]
+     */
+    protected $forNow = [];
     /**
      * Messages for next request
      *
      * @var string[]
      */
     protected $forNext = [];
-
     /**
      * Message storage
      *
      * @var null|array|ArrayAccess
      */
     protected $storage;
-
     /**
      * Message storage key
      *
      * @var string
      */
     protected $storageKey = 'slimFlash';
-
     /**
      * Create new Flash messages service provider
      *
@@ -58,16 +58,14 @@ class Messages
         } else {
             throw new InvalidArgumentException('Flash messages storage must be an array or implement \ArrayAccess');
         }
-
         // Load messages from previous request
         if (isset($this->storage[$this->storageKey]) && is_array($this->storage[$this->storageKey])) {
             $this->fromPrevious = $this->storage[$this->storageKey];
         }
         $this->storage[$this->storageKey] = [];
     }
-
     /**
-     * Add flash message
+     * Add flash message for the next request
      *
      * @param string $key The key to store the message under
      * @param mixed  $message Message to show on next request
@@ -78,11 +76,23 @@ class Messages
         if (!isset($this->storage[$this->storageKey][$key])) {
             $this->storage[$this->storageKey][$key] = array();
         }
-
         //Push onto the array
         $this->storage[$this->storageKey][$key][] = $message;
     }
-
+    /**
+     * Add flash message for current request
+     *
+     * @param string $key The key to store the message under
+     * @param mixed  $message Message to show on next request
+     */
+    public function addMessageNow($key, $message){
+        //Create Array for this key
+        if(!isset($this->forNow[$key])){
+            $this->forNow[$key] = array();
+        }
+        //Push onto the array
+        $this->forNow[$key][] = $message;
+    }
     /**
      * Get flash messages
      *
@@ -90,9 +100,17 @@ class Messages
      */
     public function getMessages()
     {
-        return $this->fromPrevious;
+        $messages = $this->fromPrevious;
+        foreach($this->forNow as $key => $values){
+            if(!isset($messages[$key])){
+                $messages[$key] = array();
+            }
+            foreach($values as $value){
+                array_push($messages[$key], $value);
+            }
+        }
+        return $messages;
     }
-
     /**
      * Get Flash Message
      *
@@ -101,7 +119,8 @@ class Messages
      */
     public function getMessage($key)
     {
+        $messages = $this->getMessages();
         //If the key exists then return all messages or null
-        return (isset($this->fromPrevious[$key])) ? $this->fromPrevious[$key] : null;
+        return (isset($messages[$key])) ? $messages[$key] : null;
     }
 }
